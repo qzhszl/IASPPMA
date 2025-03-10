400c4a2c88834a02ac01e7801789a3df6820281f
zqiu1 <Z.Qiu-1@tudelft.nl>
2025-03-10 16:37:51 +0100

    haha

---
+++ plot_simu_not_exact_Dis.m
@@ -0,0 +1,40 @@
+clear,clc
+N_vec = [10,20,50,100];
+data_mean = zeros(length(N_vec),6);
+data_std = zeros(length(N_vec),6);
+count = 1;
+for N = N_vec
+    filename = sprintf("D:\\data\\ISPP_givenA\\complete_random_demand\\LPvsQiu_N%d.txt",N);
+    results = readmatrix(filename);
+    mean_values = mean(results);
+    std_values = std(results);
+    data_mean(count,:) = mean_values;
+    data_std(count,:) = std_values;
+    count = count+1;
+end
+
+fig = figure; hold on;
+colors = lines(6); % 获取 6 种不同的颜色
+
+x = 1:4;
+for i = 1:6
+    errorbar(x, data_mean(:,i), data_std(:,i), 'o-', 'Color', colors(i,:), 'LineWidth', 3, 'MarkerSize', 8);
+end
+
+% 图像美化
+ax = gca;  % Get current axis
+ax.FontSize = 14;  % Set font size for tick label
+xlim([0.7 4.5])
+ylim([0.23 0.65])
+xticks([1 2 3 4])
+xticklabels({'10','20','50','100'})
+xlabel('$N$',Interpreter='latex',FontSize=20);
+% ylabel('$\frac{\sum_{i}\sum_{j}|d_{ij}-s_{ij}|}{\sum_{i}\sum_{j}d_{ij}}$','interpreter','latex',FontSize=20)
+ylabel('$\frac{u \cdot |D-S|\cdot u^T}{u \cdot D \cdot u^T}$','interpreter','latex',FontSize=24)
+legend({'LP', 'bn = $2$', 'bn = $0.25L$', 'bn = $0.50L$', 'bn = $0.75L$', 'bn = $L$'}, 'interpreter','latex','Location', 'northeast',FontSize=18);
+set(legend, 'Position', [0.64, 0.66, 0.2, 0.08]);
+box on
+hold off
+
+picname = sprintf("D:\\data\\ISPP_givenA\\complete_random_demand\\NotExactsolutionLPvsQiu.pdf");
+exportgraphics(fig, picname,'BackgroundColor', 'none','Resolution', 300);
