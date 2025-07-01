307d5a03c3c3f62a233d47d1a116652d48918912
zqiu1 <Z.Qiu-1@tudelft.nl>
2025-07-01 21:50:21 +0200

    20250701HAHA

--- plot_scheduled_instances.m
+++ plot_scheduled_instances.m
@@ -2,10 +2,12 @@ clear,clc
 
 % this.m plot the time simulations
 % LPvsQiu_N%dPerturbation1havetime
+% LPvsQiu_N%dhavetime
 % LPvsQiu_N%dhavetimetest
 
 
 N_vec = [10,20,50,100];
+N_vec = [20];
 data_shecduled_instace = zeros(length(N_vec),6);
 colors = ["#D08082", "#C89FBF", "#62ABC7", "#7A7DB1", "#6FB494", "#D9B382"];
 time_window = [0.001, 0.001937, 0.003753, 0.007272, 0.014092, 0.027308, 0.052912, 0.10247, 0.19837, 0.38404, 0.74346, 1.4384, 2.7826, 5.3849, 10.418, 20.173, 30];
@@ -17,7 +19,7 @@ for N = N_vec
     count = 1;
     data_shecduled_instace = zeros(length(time_window),6);
     for time_shreshold = time_window
-        filename = sprintf("D:\\data\\ISPP_givenA\\complete_random_demand\\LPvsQiu_N%dPerturbation1havetime.txt",N);
+        filename = sprintf("D:\\data\\ISPP_givenA\\complete_random_demand\\LPvsQiu_N%dhavetime.txt",N);
         result = readmatrix(filename);
         results = result(:,7:12);
         data_shecduled_instace(count,:) = sum(results<=time_shreshold,1); 
@@ -28,7 +30,7 @@ for N = N_vec
 
     fig = figure; hold on;
     
-    for i = 1:6
+    for i = 1:2
         if i==1
             plot(time_window, data_shecduled_instace(:,i)./1000, 's-', 'Color', colors(i), 'LineWidth', 3.5, 'MarkerSize', 10);
         else
@@ -55,6 +57,13 @@ for N = N_vec
     % end
 
 
+    if N == 20
+        lgd = legend({'LPLW', '$b_n = 2$'}, 'interpreter','latex','Location', 'northeast',FontSize=24);
+        set(legend, 'Position', [0.54, 0.5, 0.2, 0.08]);
+    end
+
+
+
     % if N> 50
     %     set(legend, 'Position', [0.28, 0.55, 0.2, 0.08]);
     % else
@@ -64,6 +73,6 @@ for N = N_vec
     box on
     hold off
     
-    picname = sprintf("D:\\data\\ISPP_givenA\\complete_random_demand\\PerturbationLPvsQiuScheduledcase_N%d.pdf",N);
+    picname = sprintf("D:\\data\\ISPP_givenA\\complete_random_demand\\NotExactsolutionLPvsQiuScheduledcasebn2only_N%d.pdf",N);
     exportgraphics(fig, picname,'BackgroundColor', 'none','Resolution', 600);
 end
